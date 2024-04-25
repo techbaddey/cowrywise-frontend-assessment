e7edf7a2493fc4390007bdef1e8c3e576385bc93
techbaddey <oyinkansolashoroye@gmail.com>
2024-04-25 16:22:56 +0100

    removed comments

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,11 @@ export const useStore = defineStore('store', {
     async fetchFunds() {
       try {
         const response = await axios.get('https://dashboard.cowrywise.com/api/v2/funds/public/')
-        console.log('Funds data:', response.data) 
         this.funds = response.data
-        this.error = null // Reset error if successful
+        this.error = null 
       } catch (error) {
         console.error('Error fetching funds:', error)
-        this.error = error.message // Store error message
+        this.error = error.message 
       }
     }
   }
